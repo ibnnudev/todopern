fdfaaafebfc2a13a481f964005d5507892bd1482
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2024-04-30 22:25:27 +0700

    feat: add database and get all, get by id

--- server/index.js
+++ server/index.js
@@ -1,5 +1,37 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const pool = require("./db");
+
+// middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes-----
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+app.get("/todos/:id", async (req, res) => {
+  // req params akan mengambil data pada url dengan prefix ":{nama_data}"
+  const { id } = req.params;
+  try {
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 app.listen(5000, () => {
   console.log("server has running in port 5000");
